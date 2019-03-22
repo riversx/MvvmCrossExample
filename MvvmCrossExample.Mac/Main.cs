010e4619d1aeaa55c6427d5e5e5c6808c6fdceff
Boris Sclauzero <boris.sclauzero@zupit.it>
2019-03-22 22:18:12 +0100

    adds Bugsnag

--- MvvmCrossExample.Mac/Main.cs
+++ MvvmCrossExample.Mac/Main.cs
@@ -1,13 +1,18 @@
 ﻿using AppKit;
+using Bugsnag;
 
 namespace MvvmCrossExample.Mac
 {
     static class MainClass
     {
+        private static Client BugSnag;
+
         static void Main(string[] args)
         {
+            BugSnag = new Client(new Configuration("62f35faf0de664259e3eda1cfdd8302d"));
             NSApplication.Init();
             NSApplication.Main(args);
         }
     }
+
 }

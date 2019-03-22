d43e563465513b4c5ea13d1c44b52f1fae7c9cbc
Boris Sclauzero <boris.sclauzero@zupit.it>
2019-03-22 22:45:03 +0100

    adds Bugsnag to Wpf project

--- MvvmCrossExample.Wpf/App.xaml.cs
+++ MvvmCrossExample.Wpf/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using MvvmCross.Core;
 using MvvmCross.Platforms.Wpf.Core;
 using MvvmCross.Platforms.Wpf.Views;
+using Bugsnag;
+using System.Windows;
 
 namespace MvvmCrossExample.Wpf
 {
@@ -9,9 +11,17 @@ namespace MvvmCrossExample.Wpf
     /// </summary>
     public partial class App : MvxApplication
     {
+        private static Client BugSnag;
+
         public App()
         {
             this.RegisterSetupType<MvxWpfSetup<Core.App>>();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            BugSnag = new Client(new Configuration("62f35faf0de664259e3eda1cfdd8302d"));
         }
     }
 }
